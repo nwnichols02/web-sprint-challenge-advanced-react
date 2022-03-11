39e68977122de8f84050c2b35cf36a867660407f
Nathan Nichols <nwnichols02@gmail.com>
2022-03-11 11:18:05 -0700

    functional grid is working, need to DRY and write tests

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -75,33 +75,38 @@ export default function AppFunctional(props) {
     }   
   }
 
-  // const x1 = grid.x === 1;
-  // const x2 = grid.x === 2;
-  // const x3 = grid.x === 3;
-  // const y1 = grid.y === 1;
-  // const y2 = grid.y === 2;
-  // const y3 = grid.y === 3;
-  // const sQA = "square active";
-  // const sQ = "square";
+  const resetGrid = () => {
+    setCounter(0)
+    setGrid({'x': 2, 'y': 2})
+    setMessage('')
+    setEmail('')
+  }
+
+  const x1 = grid.x === 1;
+  const x2 = grid.x === 2;
+  const x3 = grid.x === 3;
+  const y1 = grid.y === 1;
+  const y2 = grid.y === 2;
+  const y3 = grid.y === 3;
+  const sQA = "square active";
+  const sQ = "square";
 
   return (
-
-
     <div id="wrapper" className={props.className}>
       <div className="info">
         <h3 id="coordinates">Coordinates ({grid.x}, {grid.y})</h3>
-        <h3 id="steps">You moved {counter} times</h3>
+        <h3 id="steps">You moved {counter}{' '}{counter === 1 ? 'time' : 'times'}</h3>
       </div>
       <div id="grid">
-      <div className={`${grid.x == 1 && grid.y == 1 ? "square active" : "square"}`}>{grid.x === 1 && grid.y === 1 ? "B" : ""}</div>
-        <div className={`${grid.x == 2 && grid.y == 1 ? "square active" : "square"}`}>{grid.x === 2 && grid.y === 1 ? "B" : ""}</div>
-        <div className={`${grid.x == 3 && grid.y == 1 ? "square active" : "square"}`}>{grid.x === 3 && grid.y === 1 ? "B" : ""}</div>
-        <div className={`${grid.x == 1 && grid.y == 2 ? "square active" : "square"}`}>{grid.x === 1 && grid.y === 2 ? "B" : ""}</div>
-        <div className={`${grid.x == 2 && grid.y == 2 ? "square active" : "square"}`}>{grid.x === 2 && grid.y === 2 ? "B" : ""}</div>
-        <div className={`${grid.x == 3 && grid.y == 2 ? "square active" : "square"}`}>{grid.x === 3 && grid.y === 2 ? "B" : ""}</div>
-        <div className={`${grid.x == 1 && grid.y == 3 ? "square active" : "square"}`}>{grid.x === 1 && grid.y === 3 ? "B" : ""}</div>
-        <div className={`${grid.x == 2 && grid.y == 3 ? "square active" : "square"}`}>{grid.x === 2 && grid.y === 3 ? "B" : ""}</div>
-        <div className={`${grid.x == 3 && grid.y == 3 ? "square active" : "square"}`}>{grid.x === 3 && grid.y === 3 ? "B" : ""}</div>
+          <div className={x1 && y1 ? sQA : sQ}>{x1 && y1 ? "B" : ""}</div>
+          <div className={x2 && y1 ? sQA : sQ}>{x2 && y1 ? "B" : ""}</div>
+          <div className={x3 && y1 ? sQA : sQ}>{x3 && y1 ? "B" : ""}</div>
+          <div className={x1 && y2 ? sQA : sQ}>{x1 && y2 ? "B" : ""}</div>
+          <div className={x2 && y2 ? sQA : sQ}>{x2 && y2 ? "B" : ""}</div>
+          <div className={x3 && y2 ? sQA : sQ}>{x3 && y2 ? "B" : ""}</div>
+          <div className={x1 && y3 ? sQA : sQ}>{x1 && y3 ? "B" : ""}</div>
+          <div className={x2 && y3 ? sQA : sQ}>{x2 && y3 ? "B" : ""}</div>
+          <div className={x3 && y3 ? sQA : sQ}>{x3 && y3 ? "B" : ""}</div>
       </div>
       <div className="info">
         <h3 id="message">{message}</h3>
@@ -111,10 +116,10 @@ export default function AppFunctional(props) {
         <button onClick={moveUp} id="up">UP</button>
         <button onClick={moveRight}id="right">RIGHT</button>
         <button onClick={moveDown} id="down">DOWN</button>
-        <button id="reset">reset</button>
+        <button onClick={resetGrid} id="reset">reset</button>
       </div>
       <form onSubmit={onSubmit}>
-        <input id="email" type="email" placeholder="type email"></input>
+        <input value={email} onChange={changeInput} id="email" type="email" placeholder="type email"></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
