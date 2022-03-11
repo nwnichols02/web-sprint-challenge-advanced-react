8779b9c3b11009381b01c24c830fb8cc14f556ff
Nathan Nichols <nwnichols02@gmail.com>
2022-03-11 09:15:02 -0700

    functioning all buttons - need DRY and email form

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -19,17 +19,64 @@ export default class AppClass extends React.Component {
     // axios.post(URL, )
   }  
 
+
   moveUp = () => {
     if(this.state.grid.y > 1){
       this.setState({...this.state, 
         counter: this.state.counter + 1, 
-        grid:{...this.state.grid, y: this.state.grid.y -1},
+        grid:{...this.state.grid, y: this.state.grid.y - 1},
         message: ''
       })
     } else {
       this.setState({...this.state, message: `You can't go up`})
     }
   }
+
+  moveDown = () => {
+    if(this.state.grid.y < 3){
+      this.setState({...this.state,
+      counter: this.state.counter + 1,
+      grid: {...this.state.grid, y: this.state.grid.y + 1},
+      message: ''
+      })
+    } else {
+      this.setState({...this.state, message: `You can't go down`})
+    }
+  }
+
+  moveLeft = () => {
+    if(this.state.grid.x > 1){
+      this.setState({...this.state,
+      counter: this.state.counter + 1,
+      grid: {...this.state.grid, x: this.state.grid.x - 1},
+      message: ''
+      })
+    } else {
+      this.setState({...this.state, message: `You can't go left`})
+    }
+  }
+
+  moveRight = () => {
+    if(this.state.grid.x < 3){
+      this.setState({...this.state,
+      counter: this.state.counter + 1,
+      grid: {...this.state.grid, x: this.state.grid.x + 1},
+      message: ''
+      })
+    } else {
+      this.setState({...this.state, message: `You can't go right`})
+    }
+  }
+  
+  resetGrid = () => {
+    this.setState({...this.state,
+      counter: 0,
+      grid: {'x':2, 'y':2},
+      message: '',
+      email: '',
+    })
+  }
+
 
   render() {
     const { className } = this.props
@@ -62,11 +109,11 @@ export default class AppClass extends React.Component {
           <h3 id="message">{this.state.message}</h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
+          <button onClick={this.moveLeft} id="left">LEFT</button>
           <button onClick={this.moveUp} id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button onClick={this.moveRight} id="right">RIGHT</button>
+          <button onClick={this.moveDown} id="down">DOWN</button>
+          <button onClick={this.resetGrid} id="reset">reset</button>
         </div>
         <form onSubmit={this.onSubmit}>
           <input id="email" type="email" placeholder="type email"></input>
